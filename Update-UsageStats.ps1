467bf3f8cd30ef192bc11116b245a14fa38e8650
Damon Murdoch <damonmurdoch1@gmail.com>
2025-06-08 00:27:57 +1000

    Update Update-UsageStats.ps1

--- Update-UsageStats.ps1
+++ Update-UsageStats.ps1
@@ -1,3 +1,14 @@
+Param(
+    # Commit message for the upstream sub-repository
+    [Alias()][Parameter(Mandatory=$False)][String]$Commit, 
+    # Path of the folder to remote into (default: 'usage')
+    [Alias()][Parameter(Mandatory=$False)][String]$Path = "./usage", 
+    # Upstream branch to push changes to (default: 'dev')
+    [Alias()][Parameter(Mandatory=$False)][String]$Remote = 'origin', 
+    # Branch to check out & push changes to (default: 'dev')
+    [Alias()][Parameter(Mandatory=$False)][String]$Branch = 'dev'
+);
+
 # Get current path
 $Location = Get-Location;
 
@@ -5,7 +16,10 @@ $Location = Get-Location;
 Set-Location $PSScriptRoot;
 
 # Move to 'data' folder
-Set-Location "usage";
+Set-Location "$Path";
+
+# Ensure on dev branch
+& "git" "checkout" "$Branch"
 
 # Build vgc format data
 & "python" "build_vgc.py"
@@ -13,11 +27,23 @@ Set-Location "usage";
 # Compile to 'data.js'
 & "python" "compile_js.py"
 
+# Commit switch set
+If ($Commit) {
+    # Add files to repo
+    & "git" "add" "."
+
+    # Commit changes
+    & "git" "commit" "-m" "$Commit";
+
+    # Push changes
+    & "git" "push" "$Remote" "$Branch";
+}
+
 # Move back to root
 Set-Location "..";
 
 # Move 'data/data.js' to 'src/data.js' (overwrite)
-Move-Item "usage/data.js" "src/data.js" -Force;
+Move-Item "$Path/data.js" "src/data.js" -Force;
 
 # Move back to original path
 Set-Location -Path $Location;
